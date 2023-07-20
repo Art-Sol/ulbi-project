3fdc489fc4c9901b9c9cdfb90643fd00d82a0533
Art-Sol <solodushenkov-sp@mail.ru>
2023-07-20 14:59:26 +0300

    add stylelint + some translates

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,22 +2,27 @@ import React, { type FC } from 'react';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { classNames } from 'shared/lib/classNames/classNames';
 
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
   className?: string
 }
 
-export const Navbar: FC<NavbarProps> = ({ className }) => (
-  <div className={classNames(cls.navbar, {}, [className])}>
+export const Navbar: FC<NavbarProps> = ({ className }) => {
+  const { t } = useTranslation(['main', 'about']);
+  return (
 
-    <div className={cls.links}>
-      <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
-        Главная
-      </AppLink>
-      <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
-        О сайте
-      </AppLink>
+    <div className={classNames(cls.navbar, {}, [className])}>
+
+      <div className={cls.links}>
+        <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
+          {t('Главная', { ns: 'main' })}
+        </AppLink>
+        <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
+          {t('О сайте', { ns: 'about' })}
+        </AppLink>
+      </div>
     </div>
-  </div>
-);
+  );
+};
